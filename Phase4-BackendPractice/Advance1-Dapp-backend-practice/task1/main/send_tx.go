ae29baba6cc79c5ed7ccb300e97f9062a7db2eb8
shawn.zhong <shawn.zhong@mail.emis.com.tw>
2025-09-19 06:22:09 +0800

    20250919

---
+++ Phase4-BackendPractice/Advance1-Dapp-backend-practice/task1/main/send_tx.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func SendTx() {
+	client, err := ethclient.Dial("https://sepolia.infura.io/v3/59b2ab9d2eda4fa086cdd7db0e4c1a3e")
+
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+	defer client.Close()
+
+	privateKey, err := crypto.HexToECDSA("4943c8f28bd54bc4d7397aedeb250501eedb2c29ec4ef780d8637ed273339eb2")
+	if err != nil {
+		log.Fatalf("Failed to load private key: %v", err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatalf("Failed to cast public key to ECDSA: %v", err)
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatalf("Failed to get nonce: %v", err)
+	}
+
+	value := big.NewInt(10000000000000000) // 0.01 ETH
+	gasLimit := uint64(21000)              // in units
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to get gas price: %v", err)
+	}
+
+	toAddress := common.HexToAddress("0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2")
+	var data []byte
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
+	chainID := big.NewInt(11155111) // Sepolia chain ID
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatalf("Failed to sign transaction: %v", err)
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatalf("Failed to send transaction: %v", err)
+	}
+	fmt.Println("Transaction sent:", signedTx.Hash().Hex())
+}
