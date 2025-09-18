2bc45c5b2885bba5bda7c8521ad5fb86a573e634
shawn.zhong <shawn.zhong@mail.emis.com.tw>
2025-09-18 22:20:36 +0800

    add

---
+++ Phase4-BackendPractice/Advance1-Dapp-backend-practice/my-hardhat-project/contracts/Counter.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint256 private count;
+
+    event CountIncremented(uint256 newCount);
+    event CountDecremented(uint256 newCount);
+
+    function increment() public {
+        count += 1;
+        emit CountIncremented(count);
+    }
+
+    function decrement() public {
+        require(count > 0, "Counter: count cannot be negative");
+        count -= 1;
+        emit CountDecremented(count);
+    }
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+}
